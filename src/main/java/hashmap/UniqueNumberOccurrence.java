107619f4576895f834e3d63ed5daaf654243a95e
Harshit Kandhway <kandhway.harshit198@gmail.com>
2022-12-03 23:28:06 +0530

    Update UniqueNumberOccurrence.java
    Use of getOrDefault method of hashmap improves memory used .
    Runtime
    5 ms
    Beats
    44.19%
    Memory
    41.9 MB
    Beats
    64.15%

--- src/main/java/hashmap/UniqueNumberOccurrence.java
+++ src/main/java/hashmap/UniqueNumberOccurrence.java
@@ -1,16 +1,8 @@
 class UniqueNumberOccurrence {
     public boolean uniqueOccurrences(int[] arr) {
         HashMap<Integer, Integer> mapper = new HashMap();
-        mapper.put(arr[0], 1);
-        for(int i=1;i<arr.length; i++)
-        {
-            if(mapper.containsKey(arr[i])){
-                int count = mapper.get(arr[i]);
-                mapper.put(arr[i] ,count+1);
-            }
-            else{
-                mapper.put(arr[i], 1);
-            }
+        for( int i : arr){
+            mapper.put(i, mapper.getOrDefault(i, 0) + 1);
         }
         Set<Integer> set = new HashSet<Integer>();
         for(int i : mapper.keySet()){
